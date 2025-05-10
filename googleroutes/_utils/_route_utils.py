2fb518162b91047f4084c7e62d0f63cf91962348
dwporges <dwporges1@sheffield.ac.uk>
2025-05-10 17:13:49 +0100

    Refactor code, fix imports

--- googleroutes/_utils/_route_utils.py
+++ googleroutes/_utils/_route_utils.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from ._file_parser import read_kml
-import folium as fm
-from ..config import get_client
 
+import folium as fm
+import googlemaps as gm
+
+from ._file_parser import read_kml
+from ..config import get_client
 
 
 def _get_polyline_from_route(route: dict) -> list[list[str]]:
@@ -12,26 +14,27 @@ def _get_polyline_from_route(route: dict) -> list[list[str]]:
     """
 
     if route is None:
-         raise ValueError('Route is None')
+        raise ValueError('Route is None')
     if not route:
-         raise ValueError('Route is empty')
+        raise ValueError('Route is empty')
     if route == {}:
-         raise ValueError('Route is empty')
-    
+        raise ValueError('Route is empty')
+
     # Get steps from route
     steps = route[0]['legs'][0]['steps']
     steps_polyline = []
 
     # Get polyline from steps
     for step in steps:
-            step_polyline = [point for point in gm.convert.decode_polyline(step['polyline']['points'])]
-            step_lats, step_lngs = zip(*[(point['lat'], point['lng']) for point in step_polyline])
-            for step_lat, step_lng in zip(step_lats, step_lngs):
-                steps_polyline.append([step_lat, step_lng])
+        step_polyline = [point for point in gm.convert.decode_polyline(step['polyline']['points'])]
+        step_lats, step_lngs = zip(*[(point['lat'], point['lng']) for point in step_polyline])
+        for step_lat, step_lng in zip(step_lats, step_lngs):
+            steps_polyline.append([step_lat, step_lng])
     return steps_polyline
 
 
-def _get_routes_from_kml(fname: str, destination: str, mode: str='walking', region: str='uk', departure_time: datetime=None, arrival_time: datetime=None) -> list[list[list[str]]]:
+def _get_routes_from_kml(fname: str, destination: str, mode: str = 'walking', region: str = 'uk',
+                         departure_time: datetime = None, arrival_time: datetime = None) -> list[list[list[str]]]:
     """
     Get routes from a KML file
     :param fname: str: KML file name
@@ -43,9 +46,10 @@ def _get_routes_from_kml(fname: str, destination: str, mode: str='walking', regi
 
     # Read KML file
     origins = read_kml(fname, transpose=True)
-    
+
     # Get routes
-    routes = [client.directions(origin=origin, destination=destination, mode=mode, region=region, alternatives=True, departure_time=departure_time, arrival_time=arrival_time) for origin in origins]
+    routes = [client.directions(origin=origin, destination=destination, mode=mode, region=region, alternatives=True,
+                                departure_time=departure_time, arrival_time=arrival_time) for origin in origins]
 
     # Get polylines from routes in list format
     routes_polyline = [_get_polyline_from_route(route) for route in routes if route != []]
@@ -53,7 +57,7 @@ def _get_routes_from_kml(fname: str, destination: str, mode: str='walking', regi
     return routes_polyline
 
 
-def _plot_routes(routes: list[list[list[str]]], mode: str='walking', output: str='routes.html') -> None:
+def _plot_routes(routes: list[list[list[str]]], mode: str = 'walking', output: str = 'routes.html') -> None:
     """
     Plot routes from a KML file
     :param routes: list[list[list[str]]]: Routes
